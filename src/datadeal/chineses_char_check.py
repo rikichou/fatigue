bce31f2b14635349bdc514d7499d468051d21724
riki <riki@tensortec.net>
2021-08-16 09:53:48 +0800

    chineses check demo

---
+++ src/datadeal/chineses_char_check.py
@@ -0,0 +1,21 @@
+import os
+import glob
+
+def is_contains_chinese(strs):
+    for _char in strs:
+        if '\u4e00' <= _char <= '\u9fa5':
+            return True
+    return False
+
+IMGS_ROOT_DIR = '/zhourui/workspace/pro/fatigue/data/video/train/fatigue_20210122_20210131'
+
+level = 2
+videos = glob.glob(os.path.join(IMGS_ROOT_DIR, '/*' * level + '.mp4'))
+
+for v in videos:
+    if is_contains_chinese(v):
+        print(v)
+        break
+
+
+
