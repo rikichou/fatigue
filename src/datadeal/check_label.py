a76b6462ddf7eb836a9f4d7dd056dbaec9ae5ea3
ruiming <zhourui@streamax.com>
2021-08-18 09:49:15 +0800

    update check_label.py

--- src/datadeal/check_label.py
+++ src/datadeal/check_label.py
@@ -13,12 +13,13 @@ def parse_args():
         '--start_idx', type=int, default=1, help='image frames start index')
     parser.add_argument(
         '--img_format', type=str, default='img_{:05}.jpg', help='image name format')
+    parser.add_argument(
+        '--facerect_filename', type=str, default='facerect.npy', help='numpy file include face bbox informations')
     args = parser.parse_args()
 
     return args
 
-def base_info_check(args, root_dir, video_prefix, img_format, total_num):
-    video_path = os.path.join(root_dir, video_prefix)
+def base_info_check(args, video_path, img_format, total_num):
 
     for i in range(total_num):
         img_path = os.path.join(video_path, img_format.format(i+args.start_idx))
@@ -26,6 +27,15 @@ def base_info_check(args, root_dir, video_prefix, img_format, total_num):
             print("ERROR! {} not in {}".format(img_format.format(i+args.start_idx), video_path))
             return False
     return True
+
+def face_rect_check(args, video_path, file_name):
+    file_path = os.path.join(video_path, file_name)
+    rect_infos = np.load(file_path, allow_pickle=True).item()
+
+    for info in rect_infos:
+        if rect_infos[info] is None:
+            print("Have no face in {}:{}".format(video_path, info))
+
 
 def main():
     args = parse_args()
@@ -37,14 +47,16 @@ def main():
             line_split = tmp_split[:2]
             fatigue_idxs = tmp_split[3]
 
-            # base info check
+            # base info get
             video_prefix = line_split[0]
             total_num = int(line_split[1])
             video_path = os.path.join(args.src_folder, video_prefix)
 
-            base_info_check(args, args.src_folder, video_prefix, args.img_format, total_num)
+            # 1, base info check (check if image missing)
+            # base_info_check(args, video_path, args.img_format, total_num)
 
-            # # face rect check
+            # 2, face rectange check (check if the have no face or face rectangle in an image)
+            face_rect_check(args, video_path, args.facerect_filename)
             # facerect_path = os.path.join(video_path, 'facerect.npy')
             # if not os.path.exists(facerect_path):
             #     print("Warning! Face rectangel not found {}".format(video_path))
