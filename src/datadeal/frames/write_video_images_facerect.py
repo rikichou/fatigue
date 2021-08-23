bd94ff65dc970bfc679d9b0c3fee8ea44ae15211
ruiming <zhourui@streamax.com>
2021-08-23 09:04:51 +0800

    add write code

--- src/datadeal/frames/write_video_images_facerect.py
+++ src/datadeal/frames/write_video_images_facerect.py
@@ -5,6 +5,7 @@ import os.path as osp
 import sys
 import warnings
 from multiprocessing import Pool
+import random
 
 import tqdm
 import cv2
@@ -16,9 +17,13 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser(description='extract optical flows')
-    parser.add_argument('src_dir', type=str, help='source video directory')
+    parser.add_argument('src_dir', type=str, help='source video directory, if --random is set, src_dir is parent dir of video dir')
     parser.add_argument('out_dir', type=str, help='output frames directory')
-
+    parser.add_argument(
+        '--random',
+        action='store_true',
+        default=False,
+        help='whether to random choose a video dir')
     args = parser.parse_args()
 
     return args
@@ -30,12 +35,17 @@ if __name__ == '__main__':
         print(f'Creating folder: {args.out_dir}')
         os.makedirs(args.out_dir)
 
-    label_file_path = os.path.join(args.src_dir, 'facerect.npy')
+    if args.random:
+        fs = os.listdir(args.src_dir)
+        video_dir = os.path.join(args.src_dir, random.choice(fs))
+    else:
+        video_dir = args.src_dir
+    label_file_path = os.path.join(video_dir, 'facerect.npy')
     infos_dict = np.load(label_file_path, allow_pickle=True).item()
 
     for item in infos_dict:
         sx,sy,ex,ey = infos_dict[item]
-        img_path = os.path.join(args.src_dir, item)
+        img_path = os.path.join(video_dir, item)
         #image = cv2.imread(img_path)
         image = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), 1)
 
